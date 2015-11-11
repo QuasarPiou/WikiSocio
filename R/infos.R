817e255204b73f99ee1929f253cbe99cf60a6bab
cafeine05 <joubert.leo@gmail.com>
2015-11-11 13:11:47 +0100

    Adding selectRandom

--- R/infos.R
+++ R/infos.R
@@ -1,4 +1,4 @@
-#' export
+#' @export
 
 anonUser <- function(page,domaine="fr") {
 
@@ -19,7 +19,7 @@ anonUser <- function(page,domaine="fr") {
     
 }
 
-#' export
+#' @export
 
 nbLinks <- function(page,domaine="fr",namespace="0") {
     
@@ -27,4 +27,55 @@ nbLinks <- function(page,domaine="fr",namespace="0") {
     result<-length(list)
     return(result)
 
+}
+
+#' @export
+
+selectRandom <- function(nb,domaine="fr",namespace="0") {
+    
+    # Découpe du nombre en multiple de 500 (division euclidienne)
+    quotient<-nb%/%500
+    reste<-nb%%500
+    # Requête
+    result<-vector()
+
+    ## On boucle pour prendre les multiples de 500
+    if(quotient>0) {
+    for(i in 1:quotient) {
+        query<-list(
+            action="query",
+            list="random",
+            format="json",
+            rnlimit="max",
+            rnnamespace=namespace)
+  
+    exec<-GET(paste("https://",domaine,".wikipedia.org/w/api.php",sep=""),query=query)
+    content<-content(exec,"parsed")
+    content<-content[[4]][[1]]
+    content<-sapply(content,function(x){x$title})
+    result<-c(result,content)
+    }
+    }
+    ## On prend les restes
+    if(reste!=0) {
+        query<-list(
+            action="query",
+            list="random",
+            format="json",
+            rnlimit=reste,
+            rnnamespace=namespace)
+  
+        exec<-GET(paste("https://",domaine,".wikipedia.org/w/api.php",sep=""),query=query)
+        content<-content(exec,"parsed")
+        content<-content[["query"]][[1]]
+        content<-sapply(content,function(x){x$title})
+        result<-c(result,content)
+  
+    # Il faut aussi filtrer pour voir si l'on a des doublons
+    result<-unique(result)
+        if(length(result)!=nb) {
+            print(paste("Le vecteur est de longueur inférieure à",nb,". Nombre de pages vraiment sélectionnées :",length(result),sep=" "))
+        }
+    }
+    return(result)
 }
